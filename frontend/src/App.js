1a683f9d5a100fad8664c45de37479c9b76ef9df
pnaskardev <priyanshunaskar89@gmail.com>
2023-07-17 21:39:04 +0530

    added frontend data we will work on it tomorrow

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  const handleGetData = () => {
+    // Handle logic for getting data
+  };
+
+  const handleUploadData = () => {
+    // Handle logic for uploading data
+  };
+
+  return (
+    <div class="App">
+      <button onClick={handleGetData}>Get Data</button>
+      <button onClick={handleUploadData}>Upload Data</button>
+    </div>
+  );
+}
+
+export default App;
