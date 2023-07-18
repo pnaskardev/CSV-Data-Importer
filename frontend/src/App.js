87fc460f2cc7dbd7e5bf2e997fbd5e6a459a1c2b
pnaskardev <priyanshunaskar89@gmail.com>
2023-07-18 11:40:37 +0530

    frontend has been added we have to migrate from fast api to DRF

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,63 @@
+/* eslint-disable no-unused-expressions */
+import {useState} from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
-  const handleGetData = () => {
-    // Handle logic for getting data
-  };
+  // const handleGetData = () => {
+  //   // Handle logic for getting data
+  // };
 
-  const handleUploadData = () => {
+
+  const [files ,setFiles]=useState(null);
+  const [progress ,setProgress]=useState({started:false,pc:0});
+  const [msg,setMsg]=useState(null);
+
+  const handleUpload = () => {
     // Handle logic for uploading data
+    if(!files)
+    {
+      setMsg("No file selected");
+      return;
+    }
+    const fd=new FormData();
+    for(let i=0;i<files.length;i++)
+    {
+      fd.append(`file${i+1}`,files[i]);
+    }
+   
+
+    setMsg("Uploading...")
+    setProgress(prevState=>{
+      return {...prevState, started:true}
+    })
+    axios.post('http://localhost:8000/upload',fd,{
+      onUploadProgress:(progressEvent)=>
+      {
+        setProgress(prevState=>{
+          return {...prevState,pc:progressEvent.progress*100}
+        })
+      },
+      headers:{
+        "Custom-Header":"value",
+      }
+    }).then(res=>{
+      setMsg("Upload succesfylly")
+      console.log(res.data)
+    })
+    .catch(err=>{
+      setMsg(err)
+      console.log(err)
+    })
+
   };
 
   return (
     <div class="App">
-      <button onClick={handleGetData}>Get Data</button>
-      <button onClick={handleUploadData}>Upload Data</button>
+      <input onChange={(e)=>{setFiles(e.target.files)}} type="file" multiple/>
+      <button onClick={handleUpload}>Upload</button>
+      {progress.started && <progress max="100" value={progress.pc}></progress>}
+      {msg && <span>{msg}</span>}
     </div>
   );
 }
